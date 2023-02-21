05e29a5e3218c3e99dd3781a11be30e19609f01a
sourav-ojha <subhamojha9@gmail.com>
2023-02-21 19:31:54 +0530

    initial commit

---
+++ new/index.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById("renderCanvas"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
+const loadScene = () => {
+  // get query string
+  const urlParams = new URLSearchParams(window.location.search);
+  const sceneId = urlParams.get("id");
+  console.log("sceneId", sceneId);
+  if (sceneId === null) {
+    return createScene1();
+  }
+  // load scene
+  // const scene = require(`./scene/scene${sceneId}.js`);
+  return sceneId === "1" ? createScene1() : createScene2();
+  // const scene = CreateScene1();
+};
+
+const scene = loadScene(); //Call the createScene function
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+  scene.render();
+});
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+  engine.resize();
+});
